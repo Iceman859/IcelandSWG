e35de6db0a37aa4faa6fa419ce219a6e4f3f9c66
Iceman <dodgechargerrt30@gmail.com>
2023-08-28 05:18:16 -0400

    initial push

---
+++ MMOCoreORB/bin/scripts/mobile/faction/rebel/rebel_specforce_urban_guerrilla.lua
@@ -0,0 +1,69 @@
+rebel_specforce_urban_guerrilla = Creature:new {
+	objectName = "@mob/creature_names:rebel_specforce_guerrilla",
+	randomNameType = NAME_GENERIC,
+	randomNameTag = true,
+	mobType = MOB_NPC,
+	socialGroup = "rebel",
+	faction = "rebel",
+	level = 35,
+	chanceHit = 0.4,
+	damageMin = 305,
+	damageMax = 320,
+	baseXp = 3465,
+	baseHAM = 9000,
+	baseHAMmax = 10000,
+	armor = 0,
+	resists = {40,20,20,50,50,50,50,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE + OVERT,
+	creatureBitmask = PACK + KILLER + NOINTIMIDATE + NODOT,
+	optionsBitmask = AIENABLED,
+	diet = HERBIVORE,
+	scale = 1.5,
+
+	templates = {
+		"object/mobile/dressed_rebel_specforce_guerilla_human_male_01.iff",
+		"object/mobile/dressed_rebel_specforce_guerilla_moncal_male_01.iff",
+		"object/mobile/dressed_rebel_specforce_guerrilla_human_female_01.iff",
+		"object/mobile/dressed_rebel_specforce_guerrilla_rodian_female_01.iff",
+		"object/mobile/dressed_rebel_specforce_guerrilla_rodian_male_01.iff",
+		"object/mobile/dressed_rebel_specforce_guerrilla_zabrak_female_01.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "color_crystals", chance = 100000},
+				{group = "junk", chance = 4700000},
+				{group = "rifles", chance = 1000000},
+				{group = "pistols", chance = 1000000},
+				{group = "melee_weapons", chance = 1000000},
+				{group = "carbines", chance = 1000000},
+				{group = "clothing_attachments", chance = 100000},
+				{group = "armor_attachments", chance = 100000},
+				{group = "wearables_common", chance = 1000000}
+			}
+		}
+	},
+
+	-- Primary and secondary weapon should be different types (rifle/carbine, carbine/pistol, rifle/unarmed, etc)
+	-- Unarmed should be put on secondary unless the mobile doesn't use weapons, in which case "unarmed" should be put primary and "none" as secondary
+	primaryWeapon = "rebel_carbine",
+	secondaryWeapon = "stormtrooper_sword",
+	conversationTemplate = "",
+	reactionStf = "@npc_reaction/military",
+	personalityStf = "@hireling/hireling_military",
+
+	-- primaryAttacks and secondaryAttacks should be separate skill groups specific to the weapon type listed in primaryWeapon and secondaryWeapon
+	-- Use merge() to merge groups in creatureskills.lua together. If a weapon is set to "none", set the attacks variable to empty brackets
+	primaryAttacks = merge(carbineermaster,marksmanmaster),
+	secondaryAttacks = merge(fencermaster,brawlermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(rebel_specforce_urban_guerrilla, "rebel_specforce_urban_guerrilla")
